fd259ffd562a4637f11af210d57ac06d8808e7ae
saha-KUSHAL <kushal.saha@outlook.com>
2025-04-29 20:52:25 +0530

    Refactor : Add dashboard template

--- client/admin/dashboard.php
+++ client/admin/dashboard.php
@@ -1,28 +1,7 @@
-<?php
+<?php 
 session_start();
-include "../config/site_config.php";
-$pageTitle = "Dashboard of $siteName Admin";
-include "../partials/header.php";
-?>
-  <div class="flex h-screen">
-    <!-- Sidebar on the left -->
-    <div class="w-64 bg-gray-800 text-white">
-      <?php include 'admin_partials/sidebar.php'; ?>
-    </div>
-
-    <!-- Main content area (navbar + content) -->
-    <div class="flex-1 flex flex-col">
-      <!-- Navbar at the top -->
-      <div class="bg-white shadow">
-        <?php include 'admin_partials/navbar.php'; ?>
-      </div>
-
-      <!-- Page content (e.g. tables, dashboard content, etc.) -->
-      <div class="p-6 overflow-auto">
-        <?php include 'admin_partials/table.php'; ?>
-      </div>
-    </div>
-  </div>
-<?php
-include "../partials/footer.php";
+ob_start();
+include "admin_partials/table.php";
+$output = ob_get_clean();
+include "pages/dashboard.html.php"
 ?>
