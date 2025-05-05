27e1352ff97fae8ea50a9d213d68a9fcd0cb7e02
Kushal Saha <krbfxofficial@gmail.com>
2025-05-05 20:47:50 +0530

    Add product grid
    Add product grid view
    Add product grid in homepage

--- client/pages/home.php
+++ client/pages/home.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 include "../config/site_config.php";
+include "../config/file_config.php";
 
 $pageTitle = 'Home : TooCart';
 
@@ -9,7 +10,7 @@ include "../partials/navbar.php";
 
 ?>
 
-<section class="bg-white py-8 antialiased dark:bg-gray-900 md:py-16">
+<section class="hero">
     <div class="mx-auto grid max-w-screen-xl px-4 pb-8 md:grid-cols-12 lg:gap-12 lg:pb-16 xl:gap-0">
       <div class="content-center justify-self-start md:col-span-7 md:text-start">
         <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight dark:text-white md:max-w-2xl md:text-5xl xl:text-6xl">Limited Time Offer!<br />Up to 50% OFF!</h1>
@@ -17,7 +18,7 @@ include "../partials/navbar.php";
         <a href="#" class="inline-block rounded-lg bg-primary-700 px-6 py-3.5 text-center font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Shop Now</a>
       </div>
       <div class="hidden md:col-span-5 md:mt-0 md:flex">
-        <img class="dark:hidden h-[28.125rem]" src="<?= $baseUrl."client/assets/images/MOD - KI SMS 12.webp"?>" alt="Power tool illustration" />
+        <!-- <img class="dark:hidden h-[28.125rem]" src="<?= $baseUrl."client/assets/images/MOD - KI SMS 12.webp"?>" alt="Power tool illustration" /> -->
         <img class="hidden dark:block" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/girl-shopping-list-dark.svg" alt="shopping illustration" />
       </div>
     </div>
@@ -105,5 +106,13 @@ include "../partials/navbar.php";
   </section>
 
 <?php
+include_once $root."client/config/db_config.php";
+$select_query = "SELECT `id`,`product_title`, `product_price`, `product_image` FROM `product`";
+$results = mysqli_query($conn,$select_query);
+$products = array();
+while($row = mysqli_fetch_assoc($results)){
+  $products[] = $row;
+}
+include_once "product/product_grid.php";
 include "../partials/footer.php";
 ?>
