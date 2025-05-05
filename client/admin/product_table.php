d9caa2da658706f0b1bd8f66cdd05b386986d45b
saha-KUSHAL <kushal.saha@outlook.com>
2025-05-05 08:00:34 +0530

    Chore : Make product table image in start

--- client/admin/product_table.php
+++ client/admin/product_table.php
@@ -3,8 +3,8 @@ session_start();
 require_once "../config/db_config.php";
 require_once "../config/site_config.php";
 
-$table_heads = ['Category', 'Product', 'Description', 'Price', 'Brand', 'Image'];
-$select_query = "SELECT product.product_title, categories.category_title, product.product_desc, product.product_price, product.product_price, product.product_brand,product.product_image FROM product JOIN categories ON product.id = categories.id";
+$table_heads = ['Image','Category', 'Product', 'Description', 'Price', 'Brand'];
+$select_query = "SELECT product.product_image, product.product_title, categories.category_title, product.product_desc, product.product_price, product.product_price, product.product_brand FROM product JOIN categories ON product.category_id = categories.id";
 // get all the record
 $results = mysqli_query($conn, $select_query);
 while ($rows = mysqli_fetch_assoc($results)) {
