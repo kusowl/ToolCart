e8958c58cdc270db68e8fa96e53944ae28ed685c
Kushal Saha <krbfxofficial@gmail.com>
2025-05-11 12:52:47 +0530

    fix : update quantity of product when re adding it in cart

--- client/pages/product/add_to_cart.php
+++ client/pages/product/add_to_cart.php
@@ -11,21 +11,30 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
         header('Location:../home.php');
     }
     // Check if the session is set or not
-    $user_id = $_SESSION["id"] ?? ''; 
+    $user_id = $_SESSION["id"] ?? '';
     if (empty($user_id)) {
         // if user is not logged in the redirect to home page
         header('Location:../home.php');
     }
 
-    // if the product does not exist
     $select_query = "SELECT `created_at` FROM `product` WHERE `id` = $product_id";
     $result = mysqli_query($conn, $select_query);
+    // if the product does not exist
     if (mysqli_num_rows($result) == 0) {
         // redirect back to the home page
         header('Location:../home.php');
     }
 
-    $insert_query = "INSERT INTO `cart`(`product_id`, `user_id`) VALUES ('$product_id','$user_id')";
-    mysqli_query($conn, $insert_query);
+    // Check if the product is already in the cart
+    $select_query = "SELECT * FROM `cart` WHERE product_id ='" . $product_id . "'";
+    $result = mysqli_query($conn, $select_query);
+    if (mysqli_num_rows($result) > 0) {
+        // product exists then update quantity
+        $update_query = "UPDATE `cart` SET `qty`= `qty` + 1 WHERE `product_id` ='" . $product_id . "'";
+        $result = mysqli_query($conn, $update_query);
+    } else {
+        $insert_query = "INSERT INTO `cart` (`product_id`, `user_id`) VALUES ('$product_id','$user_id')";
+        mysqli_query($conn, $insert_query);
+    }
     header('Location:../home.php');
 }
