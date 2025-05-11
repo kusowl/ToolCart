05a602c311265de3e6a01bad83941ce409e564d5
Kushal Saha <krbfxofficial@gmail.com>
2025-05-11 10:17:44 +0530

    Refactored product view

--- client/pages/product/product_view.php
+++ client/pages/product/product_view.php
@@ -2,6 +2,6 @@
 <section class="max-w-screen-xl px-4 mx-auto 2xl:px-0 py-4">
 <?php 
 include_once '../partials/breadcrumb.php';
-include_once 'product_details.php';
+include_once 'product_details.html.php';
 ?>
 </section>
